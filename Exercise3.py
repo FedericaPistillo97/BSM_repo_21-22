b7f291687094423be9880156b907666e42671896
Federica Pistillo <federicapistillo@Air-di-Federica.lan>
2022-03-17 09:29:13 +0100

    Exercise3

---
+++ Exercise3.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 16 10:57:41 2022
+
+@author: chiaramarzi
+"""
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+    
+def myplot(): # TO COMPLETE
+    
+
+# Common parameters    
+q0 = # TO COMPLETE
+Psa0 = # TO COMPLETE
+Psv0 = # TO COMPLETE
+Pra0 = # TO COMPLETE
+Rsv = # TO COMPLETE
+Csa = # TO COMPLETE
+Csv = # TO COMPLETE
+Cra = # TO COMPLETE
+Poffset = # TO COMPLETE
+
+# Parameters of the simulation without controllers
+Kr0 = # TO COMPLETE
+Rsa0 = # TO COMPLETE
+
+# Parameters of the simulation with controllers
+# TO COMPLETE
+
+# Time and hemorrage definition
+# TO COMPLETE
+
+# Allocation: Pressures and cardiac output without controllers
+# TO COMPLETE
+
+# Allocation: Pressures, Rsa and Kr, and cardiac output with controllers
+# TO COMPLETE
+
+for j in range(L-1):
+    # without controller
+    # TO COMPLETE
+    
+    
+    # with controllers
+    # TO COMPLETE
+    
+V_no = # TO COMPLETE
+V = # TO COMPLETE
+
+# Callo myplot function with different parameters
+
+
+
+
+
+
